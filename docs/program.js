c9bd11f566dbfecd14b652369f2c444f81a25049
lino augusto joven galindo <linoaugustojoveng@gmail.com>
2025-05-14 15:31:58 -0500

    v0.5

--- docs/program.js
+++ docs/program.js
@@ -3,4 +3,32 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
 var marker = L.marker([4.628004720962233, -74.0659032806953]).addTo(map);
+
+// Abrir el archivo Geojson 
+// Java se ejecuta de tal manera que si una linea de codigo se demora se pasa a la siguiente sin esperar
+//esto puede generar problemas cuando se requiere secuencialidad 
+
+async function  cargarPuntos(){
+
+ var miArchivo= await fetch("microondas.geojson");
+
+//convertir el contenido a json: objeto js 
+
+ var datos= await miArchivo.json();
+
+ //obtener el arreglo de la llave features que es un conjunto de objetos tipo feature
+
+ let listaFeatures= datos["features"];
+
+ for(let i=0; i<10; i++){
+
+   let misCoordenadas= listaFeatures[i]["geometry"]["coordinates"];
+   var miMarcador= L.marker(misCoordenadas);
+   miMarcador.addTo(map);
+
+       }
+
+};
+cargarPuntos();
